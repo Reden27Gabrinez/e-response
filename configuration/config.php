cc6e80f35f3a1eedef1acca288698a4560c1ef0d
GABRINEZ <reden.gabrinez@dxc.com>
2023-01-20 12:58:51 +0800

    first commit

---
+++ configuration/config.php
@@ -0,0 +1,5 @@
+<?php
+
+    session_start();
+    // $databaseURL = "https://sample-midterm-default-rtdb.firebaseio.com/";
+    $databaseURL = "https://project-1-5666c-default-rtdb.firebaseio.com/";
