14e8e02d7524d26c0f247e8eafc1165ecd531e54
Reden27Gabrinez <redengabrinez1@gmail.com>
2023-01-25 09:34:54 +0800

    update dashboard and login

--- login.php
+++ login.php
@@ -13,7 +13,8 @@
     
       <div class="wrapper">
       <section class="login-content">
-         <div class="row m-0 align-items-center bg-white vh-100">            
+         <div class="row m-0 align-items-center bg-white vh-100">   
+            <div class="col-md-3"></div>         
             <div class="col-md-6">
                <div class="row justify-content-center">
                   <div class="col-md-10">
@@ -33,37 +34,27 @@
                                  <div class="col-lg-12">
                                     <div class="form-group">
                                        <label for="email" class="form-label">Email</label>
-                                       <input type="email" name="email" class="form-control" id="email" aria-describedby="email" placeholder=" ">
+                                       <input style="border: 1px solid black;" type="email" name="email" class="form-control" id="email" aria-describedby="email" placeholder=" ">
                                     </div>
                                  </div>
                                  <div class="col-lg-12">
                                     <div class="form-group">
                                        <label for="password" class="form-label">Password</label>
-                                       <input type="password" name="password" class="form-control" id="password" aria-describedby="password" placeholder=" ">
+                                       <input style="border: 1px solid black;" type="password" name="password" class="form-control" id="password" aria-describedby="password" placeholder=" ">
                                     </div>
-                                 </div>
-                                 <div class="col-lg-12 d-flex justify-content-between">
-                                    <div class="form-check mb-3">
-                                       <input required type="checkbox" class="form-check-input" id="customCheck1">
-                                       <label class="form-check-label" for="customCheck1">Remember Me</label>
-                                    </div>
-                                    <a href="recoverpw.html">Forgot Password?</a>
                                  </div>
                               </div>
                               <div class="d-flex justify-content-center">
                                  <!-- <button type="submit" class="btn btn-primary">Sign In</button> -->
                                  <input type="submit" class="btn btn-primary" value="Sign In">
                               </div>
-                             
-                              <p class="mt-3 text-center">
-                                 Don’t have an account? <a href="sign-up.html" class="text-underline">Click here to sign up.</a>
-                              </p>
+
                            </form>
                         </div>
                      </div>
                   </div>
                </div>
-               <div class="sign-bg">
+               <!-- <div class="sign-bg">
                   <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <g opacity="0.05">
                      <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF"/>
@@ -72,11 +63,12 @@
                      <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF"/>
                      </g>
                   </svg>
-               </div>
+               </div> -->
             </div>
-            <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
+            <div class="col-md-3"></div>   
+            <!-- <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
                <img src="assets/images/auth/01.png" class="img-fluid gradient-main animated-scaleX" alt="images">
-            </div>
+            </div> -->
          </div>
       </section>
       </div>
